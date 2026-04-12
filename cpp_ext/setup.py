6c6d622b14d1408cca65f4a141398c1445656331
Mike Ichikawa <projects.ichikawa@gmail.com>
2026-04-12 11:55:44 -0700

    Normalize trailing whitespace

--- cpp_ext/setup.py
+++ cpp_ext/setup.py
@@ -1,16 +1,32 @@
 from setuptools import setup, Extension
+
 import pybind11
 
+
+
 ext = Extension(
+
     "rolling_stats",
+
     sources=["rolling_stats.cpp"],
+
     include_dirs=[pybind11.get_include()],
+
     extra_compile_args=["-O3", "-march=native", "-std=c++17"],
+
     language="c++",
+
 )
 
+
+
 setup(
+
     name="rolling_stats",
+
     version="0.1.0",
+
     ext_modules=[ext],
+
 )
+
